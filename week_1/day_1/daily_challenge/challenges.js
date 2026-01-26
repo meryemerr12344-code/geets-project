80ff88932a3bab723ff6b0722df16b83f8e06543
code meryemerr12344 <meryemerr12344@gmail.com>
2026-01-26 16:28:58 +0100

    first commit

---
+++ week_1/day_1/daily_challenge/challenges.js
@@ -0,0 +1,5 @@
+// ====== Daily Challenge 1
+console.log("hello word");
+// ====== Daily Challenge 2
+
+// ====== Daily Challenge 3
---
+++ week_1/day_1/exercises/exercises_xp.js
@@ -0,0 +1,9 @@
+// ===== Exercise 1
+//console.log("Hello World");
+// ===== Exercise 2
+
+// ===== Exercise 3
+
+// ===== Exercise 4
+
+// ===== Exercise ...
